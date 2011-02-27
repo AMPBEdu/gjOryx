f9e98b30d17796573264342521811eb70236f484
Furyhunter <furyhunter600@gmail.com>
2011-02-27 17:51:53 -0600

    Initial import.

---
+++ src/com/oryxhatesjava/OryxApplet.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2011 Furyhunter
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the creator nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.oryxhatesjava;
+
+import java.applet.Applet;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/**
+ * <p>
+ * TODO document this type
+ * </p>
+ * <p>
+ * Started Feb 27, 2011
+ * </p>
+ * 
+ * @author Furyhunter
+ */
+public class OryxApplet extends Applet {
+    
+    public Client client;
+    
+    public OryxApplet(Client cli) {
+        client = cli;
+        
+        setPreferredSize(new Dimension(800, 600));
+    }
+    
+    public OryxApplet() {
+        this(new Client());
+    }
+    
+    /**
+     * <p>
+     * TODO document this method
+     * </p>
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        OryxApplet app = new OryxApplet();
+        JFrame frame = new JFrame("Realm of the Mad God");
+    }
+    
+}
